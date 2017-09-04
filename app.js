5aa4c3f6e1a80321570323a415d7dba8c4311876
Tchoupinax <corentinfiloche@hotmail.fr>
2017-09-04 08:52:31 +0200

    transfert fixe

--- app.js
+++ app.js
@@ -12,5 +12,33 @@ program
 
 
 
-let v = controller.generate("home", "./", true, ["index", "up"]);
-console.log(v)
+//let v = controller.generate("home", "./", true, ["index", "up"]);
+//console.log(v)
+var ProgressBar = require('ascii-progress');
+
+const { spawn } = require('child_process');
+const ls = spawn('git', ['clone', 'git@gitlab.com:dalvik/test-express.git']);
+
+var bar = new ProgressBar({
+    schema: '╢:bar╟ :percent :elapseds',
+    blank: '░',
+    filled: '█'
+});
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+    var iv = setInterval(function() {
+        bar.tick();
+        if (bar.completed) {
+            clearInterval(iv);
+        }
+    }, 100);
+});
+
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
